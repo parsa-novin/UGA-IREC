9e5b991247076a258b478c727044ac86ddebe488
parsanovin <pn01217@uga.edu>
2024-11-09 20:32:43 -0500

    uart test files

---
+++ esp-testing/uart_select/main/uartread.cpp
@@ -0,0 +1,12 @@
+#include <driver/uart.h>
+const uart_port_t uart_0 = UART_NUM_0;
+uart_config_t uart0_config = {
+    .baud_rate = 9600,
+    .data_bits = UART_DATA_8_BITS,
+    .parity = UART_PARITY_DISABLE,
+    .stop_bits = UART_STOP_BITS_1,
+    .flow_ctrl = UART_HW_FLOWCTRL_CTS_RTS,
+    .rx_flow_ctrl_thresh = 122,
+};
+// Configure UART parameters
+ESP_ERROR_CHECK(uart_param_config(uart_1, &uart1_config));
