b62ab47489f67f8cb7c70b59a5e1b9d81aac0d1e
Davis Jennings <davisjennings1180@gmail.com>
2025-04-16 09:00:13 -0400

    Added additional features

--- Data Analysis/BothAnalysis.py
+++ Data Analysis/BothAnalysis.py
@@ -9,8 +9,8 @@ GPSData = pd.read_csv('KJ4SAE01517_04-13-2025_12_59_18.csv')
 PrimAltData = pd.read_csv('Cloud ROSCO.csv')
 # reading csv for primary altimeter
 # print(PrimAltData.head())
-
-GPSData['UNIXTIME'] = (GPSData['UNIXTIME'] - 1744577957).round(1)
+TimeModifier = 0.5  
+GPSData['UNIXTIME'] = (GPSData['UNIXTIME'] - (1744577957 + TimeModifier)).round(1)
 # subtract a value from the UNIX time to yield a more readable value (zeroing time).
 # The starting UNIX time is 1744577953.7, I like 1744577957.0 because I see the first movement immediately afterwards.
 # THERE'S SO MUCH FLOATING POINT ERROR JESUS CHRIST
@@ -31,10 +31,6 @@ CorrectionFactor = abs(np.cos(StartingLatitude))
 GPSData['LAT'] = GPSData['LAT'] - 34.05208 
 GPSData['LON'] = GPSData['LON'] - -80.41969 # Zeroing launch pad lat and long
 
-# Insert new rows that are feet from the launch pad north/west (or whatever orthogonal direction)
-# ^ Do we super care about this besides for final landing point? ^ 
-# Determine vertical heading 
-# Determine vertical velocity with the ALT row - VERTV column exists
 # Determine true velocity by doing cubic pythagorean theorem with ALT and orthogonal directions     
 # 
 GPSData['DeltaT'] = GPSData['UNIXTIME'].diff() # calculate delta t of between entries. It changes from 0.1s to 1s after apogee detection.
@@ -63,7 +59,10 @@ GPSData['NSVelSmooth'] = GPSData['NSVel'].rolling(window=7, center=True).mean()
 # Calculate angle from vertical (zenith) based off the velocity readout
 GPSData['Zenith'] = 90 - ((np.arctan(GPSData['VERTV']/GPSData['HORZV'])) * 180/np.pi)
 
+GPSData['TotalV'] = np.sqrt(GPSData['HORZV']**2 + GPSData['VERTV']**2)
+
 GPSData.to_csv('pythonmoddedgpsdata.csv')
+PrimAltData.to_csv('pythonmoddedaltdata.csv')
 
 plt.close('all') # close all currently open figures
 
@@ -73,29 +72,87 @@ ax2 = fig1.add_subplot(3,1,2)
 ax3 = fig1.add_subplot(3,1,3)
 # ax3 = fig.add_subplot(3,1,3)
 
-ax1.plot(GPSData['UNIXTIME'], GPSData['EWVelSmooth'], label='East-West Velocity (ft/s)')
-ax1.plot(GPSData['UNIXTIME'], GPSData['NSVelSmooth'], label='North-South Velocity (ft/s)')
-ax1.plot(GPSData['UNIXTIME'], GPSData['HORZV'], label='Given Horizontal Velocity (ft/s)')
+ax1.title.set_text('Horizontal Velocities (ft/s)')
+ax1.plot(GPSData['UNIXTIME'], GPSData['EWVelSmooth'], label='East-West')
+ax1.plot(GPSData['UNIXTIME'], GPSData['NSVelSmooth'], label='North-South')
+ax1.plot(GPSData['UNIXTIME'], GPSData['HORZV'], label='Combined Horizontal')
 # ax1.plot(GPSData['UNIXTIME'], GPSData['CalcHorVelSmooth'], label='Calculated Horizontal Velocity')
+ax1.axvline(x=16.4, color='k', linestyle='--', label='Apogee') # line denoting apogee
 ax1.legend()
+ax1.grid()
 
-ax2.plot(GPSData['UNIXTIME'], GPSData['VERTV'], label='Vertical Velocity (ft/s)')
+
+
+ax2.title.set_text('Distance from Pad (ft)')
+ax2.plot(GPSData['UNIXTIME'], GPSData['EWFeet'], label='East-West')
+ax2.plot(GPSData['UNIXTIME'], GPSData['NSFeet'], label='North-South')
+ax2.plot(GPSData['UNIXTIME'], GPSData['Distance (feet)'], label='Total')
+ax2.axvline(x=16.4, color='k', linestyle='--', label='Apogee') # line denoting apogee
 ax2.legend()
+ax2.grid()
+fig1.subplots_adjust(hspace=0.5) # fixing title overlapping axis label and numbers
 
-ax3.plot(GPSData['UNIXTIME'], GPSData['EWFeet'], label='East-West Distance (ft)')
-ax3.plot(GPSData['UNIXTIME'], GPSData['NSFeet'], label='North-South Distance (ft)')
-ax3.plot(GPSData['UNIXTIME'], GPSData['Distance (feet)'], label='Total Horizontal Distance (ft)')
+ax3.title.set_text('Velocity Vector Angle')
+ax3.plot(GPSData['UNIXTIME'], GPSData['Zenith'], label='Degrees from Vertical')
+ax3.axvline(x=16.4, color='k', linestyle='--', label='Apogee') # line denoting apogee
+# ax1.axhline(y=90,color='k', linestyle='-', linewidth = 0.5)
 ax3.legend()
+ax3.grid()
+# I desire to flip the vertical axis
+ax3.invert_yaxis()
+ax3.set_xlim(0,20)
+ax3.set_yticks([0, 45, 90, 135])
+ax3.set_yticklabels(['0', '45', '90', '45'])
 
-
-# HEY THE DISTANCE FOR LONGITUDE IS PROBABLY DIFFERENT BECAUSE WE'RE NOT AT THE EQUATOR
-# Plot demonstrates that the horizontal velocity readout is a little different from what the GPS shows.
+# Plot demonstrates that the horizontal velocity readout is a little different from what the GPS recorded.
 # So, we'll just use that instead. 
 
+# FIGURE 2: Altitude and Velocity
 fig2 = plt.figure(2, figsize=(10.,10.))
-ax1 = fig2.add_subplot(3,1,1)
-ax2 = fig2.add_subplot(3,1,2)
-ax3 = fig2.add_subplot(3,1,3)
+ax1 = fig2.add_subplot(2,1,1)
+ax2 = fig2.add_subplot(2,1,2)
 
-ax1.plot(GPSData['UNIXTIME'], GPSData['Zenith'], label='Zenith Angle(degrees from vertical)')
+ax1.title.set_text('Altitude Above Ground Level (AGL) (ft)')
+ax1.plot(GPSData['UNIXTIME'], GPSData['Altitude AGL'], label='GPS')
+ax1.axvline(x=16.4, color='k', linestyle='--', label='Apogee') # line denoting apogee
+ax1.plot(PrimAltData['Time'], PrimAltData['Altitude'], label='Altimiter' )
+ax1.legend()
+ax1.grid()
+
+ax2.title.set_text('Vertical Velocity (ft/s)')
+ax2.plot(GPSData['UNIXTIME'], GPSData['VERTV'], label='GPS')
+ax2.plot(GPSData['UNIXTIME'], GPSData['TotalV'], label="Total Velocity")
+ax2.plot(PrimAltData['Time'], PrimAltData['Velocity'], label='Altimeter')
+ax2.axvline(x=16.4, color='k', linestyle='--', label='Apogee') # line denoting apogee
+ax2.legend()
+ax2.grid()
+
+
+fig2.subplots_adjust(hspace=0.45) # fixing title overlapping axis label and numbers
+
+# FIGURE 3: ZOOMED IN Altitude and Velocity
+fig3 = plt.figure(3, figsize=(10.,10.))
+ax1 = fig3.add_subplot(2,1,1)
+ax2 = fig3.add_subplot(2,1,2)
+
+ax1.title.set_text('AGL Zoomed (ft)')
+ax1.plot(GPSData['UNIXTIME'], GPSData['Altitude AGL'], label='GPS')
+ax1.axvline(x=16.4, color='k', linestyle='--', label='Apogee') # line denoting apogee
+ax1.plot(PrimAltData['Time'], PrimAltData['Altitude'], label='Altimiter' )
+ax1.set_xlim([0, 30])
+ax1.legend()
+ax1.grid()
+
+ax2.title.set_text('Vertical Velocity Zoomed (ft/s)')
+ax2.plot(GPSData['UNIXTIME'], GPSData['VERTV'], label='GPS')
+ax2.plot(PrimAltData['Time'], PrimAltData['Velocity'], label='Altimeter')
+ax2.axvline(x=16.4, color='k', linestyle='--', label='Apogee') # line denoting apogee
+# ax2.axhline(y=0,color='k', linestyle='-', linewidth = 0.5) # zero velocity
+ax2.set_xlim([0, 30])
+ax2.legend()
+ax2.grid()
+
+fig2.subplots_adjust(hspace=0.45) # fixing title overlapping axis label and numbers
+
 plt.show()
+
